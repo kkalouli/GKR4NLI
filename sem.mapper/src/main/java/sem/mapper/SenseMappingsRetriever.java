914c8b9e59421d2e1a8dcfc557261e5950d6069c
kkalouli <Aikaterini-Lida.Kalouli@uni-konstanz.de>
2019-01-18 08:35:42 +0100

    changes for gnli to compile

--- sem.mapper/src/main/java/sem/mapper/SenseMappingsRetriever.java
+++ sem.mapper/src/main/java/sem/mapper/SenseMappingsRetriever.java
@@ -3,21 +3,13 @@ package sem.mapper;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -29,17 +21,15 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+
+//import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+//import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
 
 import edu.mit.jwi.Dictionary;
 import edu.mit.jwi.IDictionary;
 import edu.mit.jwi.item.IIndexWord;
-import edu.mit.jwi.item.ILexFile;
 import edu.mit.jwi.item.ISynset;
 import edu.mit.jwi.item.ISynsetID;
 import edu.mit.jwi.item.IWord;
@@ -47,7 +37,6 @@ import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
 import edu.mit.jwi.item.Pointer;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.process.PTBTokenizer;
 import jigsaw.JIGSAW;
@@ -66,6 +55,7 @@ public class SenseMappingsRetriever {
 	public ArrayList<String> hypernyms = new ArrayList<String>();
 	public ArrayList<String> hyponyms = new ArrayList<String>();
 	public ArrayList<String> antonyms = new ArrayList<String>();
+	public double[] embed = new double[300];
 	private JIGSAW jigsaw;
 	private Properties props;
 	private String wnInstall;
@@ -396,8 +386,12 @@ public class SenseMappingsRetriever {
 	}
 	
 	
-	//public HashMap<String, String> mapNodeToEmbed(SkolemNode node, SemanticGraph graph){
-		//WordEmbeddingRelatedness.wordVectors = WordVectorSerializer.loadTxtVectors(new File(wePath));
-	//}
+	/*public void mapNodeToEmbed(SkolemNode node){
+		WordVectors glove = null;
+		String lemma = node.getStem();
+		glove = WordVectorSerializer.readWord2VecModel(new File("/Users/kkalouli/Documents/project/gnli/glove.840B.300d.txt"));
+        double[] wordVector = glove.getWordVector(lemma);
+        this.embed = wordVector;
+	}*/
 
 }
